d20c2e776dd791528727e971cf4326a327fa1706
ambuj1211 <brilliantambuj5@gmail.com>
2024-06-29 11:44:39 +0530

    adding smooth scroll in index

---
+++ scroll.js
@@ -0,0 +1,17 @@
+        // Smooth scroll with offset
+document.querySelectorAll('.nav-link, .dropdown-item').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const offset = 200;
+        const bodyRect = document.body.getBoundingClientRect().top;
+        const elementRect = targetElement.getBoundingClientRect().top;
+        const elementPosition = elementRect - bodyRect;
+        const offsetPosition = elementPosition - offset;
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
