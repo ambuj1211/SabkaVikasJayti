01fb1cd6a1da4f9d6c328a1289d5fe15e111d802
ambuj1211 <brilliantambuj5@gmail.com>
2024-07-14 10:25:49 +0530

    make photo link as cdn

--- script.js
+++ script.js
@@ -11,32 +11,32 @@ function container1(){
     if(text == a)
         {
             document.getElementById('text').innerHTML = text.replace(a,b);
-            document.getElementById('incircle').style.backgroundImage = "url(image/book.jpg)";
+            document.getElementById('incircle').style.backgroundImage = "url(https://res.cloudinary.com/dts528npw/image/upload/v1720887804/book_iffjeu.jpg)";
         }
     else if(text == b)
     {
         document.getElementById('text').innerHTML = text.replace(b,c);
-        document.getElementById('incircle').style.backgroundImage = "url(image/cows.jpg)";
+        document.getElementById('incircle').style.backgroundImage = "url(https://res.cloudinary.com/dts528npw/image/upload/v1720887803/cows_jfnumu.jpg)";
     }
     else if(text == c)
     {
         document.getElementById('text').innerHTML = text.replace(c,d);
-        document.getElementById('incircle').style.backgroundImage = "url(image/compost.jpg)";
+        document.getElementById('incircle').style.backgroundImage = "url(https://res.cloudinary.com/dts528npw/image/upload/v1720887803/compost_avtedh.jpg)";
     }
     else if(text == d)
     {
         document.getElementById('text').innerHTML = text.replace(d,e);
-        document.getElementById('incircle').style.backgroundImage = "url(image/machine.jpg)";
+        document.getElementById('incircle').style.backgroundImage = "url(https://res.cloudinary.com/dts528npw/image/upload/v1720887806/machine_pn3kwa.jpg)";
     }
     else if(text == e)
     {
         document.getElementById('text').innerHTML = text.replace(e,f);
-        document.getElementById('incircle').style.backgroundImage = "url(image/brainTech.jpg)";
+        document.getElementById('incircle').style.backgroundImage = "url(https://res.cloudinary.com/dts528npw/image/upload/v1720887805/brainTech_iy43fp.jpg)";
     }
     else if(text == f)
     {
         document.getElementById('text').innerHTML = text.replace(f,a);
-        document.getElementById('incircle').style.backgroundImage = "url(image/images.jpeg)";
+        document.getElementById('incircle').style.backgroundImage = "url(https://res.cloudinary.com/dts528npw/image/upload/v1720887805/images_s4kp3r.jpg)";
     }
     setTimeout(container1,4598);
 }
